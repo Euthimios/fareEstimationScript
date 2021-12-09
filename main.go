d177740e7986becbde97bdbf396e92999de48c9d
papalexis <papaleksis@intralot.com>
2021-12-09 22:04:18 +0200

    -- read files from cli

--- main.go
+++ main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"thaBeat/ride/farecalculation"
 	"thaBeat/ride/parser"
 	"thaBeat/utils"
 )
 
 func main() {
-	input := "files/input.csv"
-	output := "files/output.csv"
+	//input := "files/input.csv"
+	//output := "files/output.csv"
+	if len(os.Args) < 3 {
+		fmt.Println("A filepath argument is required")
+		os.Exit(1)
+	}
+	input := os.Args[1]
+	output := os.Args[2]
 
 	err := Estimator(input, output)
 	if err != nil {
